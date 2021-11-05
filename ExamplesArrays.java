9fe8db634999dd1f270eace3b434870bff98ba88
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2021-11-05 05:05:52 -0700

    Task 1 Method 3 Not Yet Tested

--- ExamplesArrays.java
+++ ExamplesArrays.java
@@ -25,7 +25,7 @@ class ExamplesArrays
 		}
 
 		double lowest = numSet[0];
-		int lowestIndex = 0; // double check that this doesn't break anything
+		int lowestIndex = 0;
 		double sum = 0;
 
 		for (int i = 0; i < numSet.length; i++)
@@ -56,8 +56,8 @@ class ExamplesArrays
 	}
 
 	/**
-	 * Takes an array of Pair and returns array of int where each element of returned
-	 * array is sum of a and b fields of corresponding pair.
+	 * Takes an array of Pair and returns array of int where each element of
+	 * returned array is sum of a and b fields of corresponding pair.
 	 *
 	 * @param pairs
 	 * @return sums
@@ -68,19 +68,63 @@ class ExamplesArrays
 
 		for (int i = 0; i < pairs.length; i++)
 		{
-System.out.println("pairs[" + i + "].a: " + pairs[i].a);
-System.out.println("pairs[" + i + "].b: " + pairs[i].b);
+//System.out.println("pairs[" + i + "].a: " + pairs[i].a);
+//System.out.println("pairs[" + i + "].b: " + pairs[i].b);
 			sums[i] = pairs[i].a + pairs[i].b;
-System.out.println("sums[" + i + "]: " + sums[i]);
+//System.out.println("sums[" + i + "]: " + sums[i]);
 		}
 
 		return sums;
 	}
 
+	/**
+	 * Takes array of Region and a Point and returns a new array of Regions
+	 * from input that contain given Point
+	 *
+	 * @param regions
+	 * @param point
+	 * @return regionsWithPoints
+	 */
+	Region[] regionsWithPoint(Region[] regions, Point point)
+	{
+		int regCount = 0;
+
+		for (int regIndex = 0; regIndex < regions.length; regIndex++)
+		{
+			if (regions[regIndex].contains(point))
+			{
+				regCount += 1;
+			}
+		}
+
+		Region[] regionsWithPoint = new Region[regCount];
+		int regWithIndex = 0;
+
+		for (int regIndex = 0; regIndex < regions.length; regIndex++)
+		{
+			if (regions[regIndex].contains(point))
+			{
+				regionsWithPoint[regWithIndex] = regions[regIndex];
+				regWithIndex += 1;
+			}
+		}
+
+		return regionsWithPoint;
+	}
+
+	void testAverageWithoutLowest(Tester t)
+	{
+		double[] unique = {1.0, 2.0, 3.0};
+		t.checkExpect(averageWithoutLowest(unique), 2.5);
+	};
+
+/*
 	// TEST
 	public static void main(String[] args)
 	{
-/*		ExamplesArrays averageTest = new ExamplesArrays();
+		ExamplesArrays test = new ExamplesArrays();
+		// TASK 1
+		ExamplesArrays averageTest = new ExamplesArrays();
 		double[] exArray = {2.0, 1.0, 1.0, 1.0};
 		double avg = averageTest.averageWithoutLowest(exArray);
 
@@ -90,8 +134,10 @@ System.out.println("sums[" + i + "]: " + sums[i]);
 		}
 		System.out.println("avg: " + avg);
 		System.out.println("lowest: " + averageTest.lowest);
-*/
-		ExamplesArrays test = new ExamplesArrays();
+
+
+		// TASK 2
+
 		Pair pair1 = new Pair(1,2);
 		Pair pair2 = new Pair(3,4);
 //		Pair pair3 = new Pair(3,3);
@@ -101,7 +147,21 @@ for (int i = 0; i < intsArray.length; i++)
 {
 	System.out.println("intsArray[" + i + "]: " + intsArray[i]);
 }
+
+
+		// TASK 3
+		Region[] regions = {new CircleRegion(new Point(0, 0), 5),
+			new CircleRegion(new Point(0, 0), 10)};
+
+		Region[] regTest = test.regionsWithPoint(regions, new Point(9, 0));
+for (int i = 0; i < regTest.length; i++)
+{
+	System.out.println
+}
+
+		
 	}
+*/
 }
 
 
